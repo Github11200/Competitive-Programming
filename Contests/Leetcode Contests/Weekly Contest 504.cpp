e9bf66f3d22597a7512290f35a1b4711c140e8f7
Github11200 <pateljinay@outlook.com>
2026-05-30 21:06:49 -0700

    did a contest

---
+++ Contests/Leetcode Contests/Weekly Contest 504.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int digitFrequencyScore(int n) {
+  int sum = 0;
+
+  while (n > 0) {
+    sum += n % 10;
+    n /= 10;
+  }
+
+  return sum;
+}
+
+int main() { return 0; }
